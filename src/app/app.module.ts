274437f02fffa1c90a3c307db32ea1d5e4aa5d33
cyrus43 <cyrusmbithi92@gmail.com>
2024-01-06 19:12:26 +0300

    feat(i18n): Added and Configured Internationalization (i18n)
    - Implemented internationalization (i18n) to support multiple languages across the application.
    - Added necessary dependencies and configurations for i18n support.
    - Updated components and templates to use translation keys and language-specific content.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
+ }
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+      }
+  }),
     AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+],
   providers: [],
   bootstrap: [AppComponent]
 })
