83d8f111654a91d4cbfad5d052e19ec10c024697
cyrus43 <cyrusmbithi92@gmail.com>
2024-01-11 11:13:12 +0300

    modified home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,41 @@
 ﻿import { Component } from '@angular/core';
 import { User } from '../_models';
 import { AccountService } from '../_services';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
-@Component({ templateUrl: 'home.component.html' })
+
+@Component({
+  templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.scss']
+})
 export class HomeComponent {
-    user: User | null;
+  user: User | null;
+  apiList: any[] = []; // Initialize as an empty array
 
-    constructor(private accountService: AccountService) {
-        this.user = this.accountService.userValue;
-    }
+  constructor(private accountService: AccountService, private http: HttpClient, private router: Router) {
+    this.user = this.accountService.userValue;
+    this.loadApiList();
+  }
+
+  // Function to load the description for Receive Payments API
+
+  // Function to load the API list
+  loadApiList() {
+    this.http.get<any>('../assets/data/apilist.json')
+      .subscribe({
+        next: (data) => {
+          this.apiList = data;
+        },
+        error: (error) => {
+          console.error('Error loading API list:', error);
+        }
+      });
+  }
+
+  // Function to handle the click event for API cards
+  onApiCardClick(api: any) {
+    // Implement logic to navigate or perform specific actions when a card is clicked
+    console.log(`${api.title} card clicked!`);
+  }
 }
